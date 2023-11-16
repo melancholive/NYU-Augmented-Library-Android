0e3f870382b8e5944ec4234bfd472953fba9af51
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2023-11-16 18:00:22 -0500

    round searchbar

--- Screens/Explore.js
+++ Screens/Explore.js
@@ -12,6 +12,8 @@ import styles from "./styles/General";
 
 export default function Explore() {
   const [searchQuery, setSearchQuery] = useState('');
+  const bords1={borderTopLeftRadius:15,borderBottomLeftRadius:15};
+  const bords2={borderTopRightRadius:15,borderBottomRightRadius:15};
   return(
     <ScreenTemplate contentContainerStyle={styles.scrollView}>
       <ScrollView>
@@ -19,12 +21,12 @@ export default function Explore() {
         <View style={{width: 300,alignSelf: 'center'}}>
           <View style={{ flexDirection: 'row', alignItems: 'center' }}>
             <TextInput
-              style={{flex: 1,height: 30,borderWidth: 1,borderColor: 'gray',borderRadius: 5,...styles.searchBar}}
+              style={{flex: 1,height: 30,paddingLeft: 7,borderWidth: 1,borderColor: 'gray',...bords1,...styles.searchBar}}
               placeholder="Search..."
               onChangeText={text=>setSearchQuery(text)}
               value={searchQuery}
             ></TextInput>
-            <Button title="Search" style={{flex: 1}}></Button>
+            <Button title="Search" style={{flex: 1,...bords2}}></Button>
           </View>
         </View>
         <View style={styles.exploreBox}><Text>Books & More</Text></View>
