264a0ee60706061be410beb3e4bbc3932eb31d70
Alice <xinranjiang2015@yahoo.com>
2023-11-20 13:04:54 -0500

    added search page prototype

--- App.js
+++ App.js
@@ -1,57 +1,62 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {createStackNavigator} from "@react-navigation/stack";
-import {NavigationContainer} from "@react-navigation/native";
-import { WelcomeScreen, Services, About,SignIn, ReserveScreen, ARVision, PickTime } from "./index";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import RoomReservationStack from "./ScreenStacks/RoomReservationStack";
-import Icon from "react-native-vector-icons/AntDesign";
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import {
+  WelcomeScreen,
+  Services,
+  About,
+  SignIn,
+  ReserveScreen,
+  ARVision,
+  PickTime,
+  SearchScreen,
+} from './index';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import RoomReservationStack from './ScreenStacks/RoomReservationStack';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const Tab = createBottomTabNavigator();
 
-
 const screenOptions = (route, color) => {
-    let iconName;
+  let iconName;
 
-    switch (route.name) {
-        case 'Home':
-            iconName = 'home';
-            break;
-        case 'Browse':
-            iconName = 'appstore-o';
-            break;
-        case 'Library':
-            iconName = 'folder1';
-            break;
-        default:
-            break;
-    }
+  switch (route.name) {
+    case 'Home':
+      iconName = 'home';
+      break;
+    case 'Browse':
+      iconName = 'appstore-o';
+      break;
+    case 'Library':
+      iconName = 'folder1';
+      break;
+    default:
+      break;
+  }
 
-    return <Icon name={iconName} color={color} size={24} />;
+  return <Icon name={iconName} color={color} size={24} />;
 };
 
 export default function App() {
-
   const stackDefault = { headerShown: false };
   return (
     <NavigationContainer>
-
       <Tab.Navigator // TODO Understand THis!!!
-          screenOptions={
-              ({route}) => ({
-                    tabBarIcon: ({color}) => screenOptions(route, color),
-                })
-          }>
-        <Tab.Screen name={"Welcome Screen"} component={WelcomeScreen} />
-          <Tab.Screen name={"SignIn"} component={SignIn} />
-          <Tab.Screen name={"About"} component={About} />
-        <Tab.Screen name={"Services"} component={Services} />
-        <Tab.Screen name={"RoomReservationStack"} component={RoomReservationStack} />
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color }) => screenOptions(route, color),
+        })}
+      >
+        <Tab.Screen name={'Welcome Screen'} component={WelcomeScreen} />
+        <Tab.Screen name={'SignIn'} component={SignIn} />
+        <Tab.Screen name={'About'} component={About} />
+        <Tab.Screen name={'Services'} component={Services} />
+        <Tab.Screen
+          name={'RoomReservationStack'}
+          component={RoomReservationStack}
+        />
+        <Tab.Screen name={'Search'} component={SearchScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
-
-
-
